135796e355270f2377242cd8b96596d7913e34d1
abhishek-mehta-dev <mehtaabhishek499@gmail.com>
2026-02-12 16:55:54 +0530

    Revert "refactor: restructure Express application into separate files"
    This reverts commit 3908e3d757d407861b894a9979b6b90b5f56ca03.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import app from "./app";
+import express, { Request, Response } from "express";
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript is working 🚀");
+});
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
